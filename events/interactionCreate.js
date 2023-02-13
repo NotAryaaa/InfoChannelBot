699d9baeea1e07790e72fa5226c085a981194225
NotArya <122024468+NotAryaaa@users.noreply.github.com>
2023-02-13 20:59:19 +0330

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,8 +6,8 @@ module.exports = (client, inter) => {
         const command = client.commands.get(inter.commandName);
 
     if (!command) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription('Error! Please contact Developers!')], ephemeral: true, }), client.slash.delete(inter.commandName)
-    if (command.permissions && !inter.member.permissions.has(command.permissions)) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`Only members with \`${Private.roleName}\` role can use **InfoChannelBot**`)], ephemeral: true, })
-    if (Private.enabled && Private.commands.includes(command) && !inter.member._roles.includes(inter.guild.roles.cache.find(x => x.name === Private.roleName).id)) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`Only members with \`${Private.roleName}\` role can use **InfoChannelBot** `)], ephemeral: true, })
+    if (command.permissions && !inter.member.permissions.has(command.permissions)) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`Only members with \`${Private.roleName}\` role can use **InfoChannelBot Setup**`)], ephemeral: true, })
+    if (Private.enabled && Private.commands.includes(command) && !inter.member._roles.includes(inter.guild.roles.cache.find(x => x.name === Private.roleName).id)) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`Only members with \`${Private.roleName}\` role can use **InfoChannelBot Setup** `)], ephemeral: true, })
         command.execute({ inter, client });
     }
     if (inter.type === InteractionType.MessageComponent) {
