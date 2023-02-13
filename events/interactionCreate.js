11c1dde51148cafe856ca34618890cc7f9f7a1b5
NotArya <122024468+NotAryaaa@users.noreply.github.com>
2023-02-13 21:10:34 +0330

    🚓 Quick Fix

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -13,7 +13,6 @@ module.exports = (client, inter) => {
     if (inter.type === InteractionType.MessageComponent) {
         const customId = JSON.parse(inter.customId)
         const file_of_button = customId.ffb
-        const queue = player.getQueue(inter.guildId);
         if (file_of_button) {
             delete require.cache[require.resolve(`../src/buttons/${file_of_button}.js`)];
             const button = require(`../src/buttons/${file_of_button}.js`)
